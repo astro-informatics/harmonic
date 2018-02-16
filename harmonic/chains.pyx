d60f634a7708882f4493384f885451a5819a7b3f
Jason McEwen <jason.mcewen@ucl.ac.uk>
2018-02-16 14:48:00 +0000

    Comment out chain pseudo code so build doesn't fail

--- harmonic/chains.pyx
+++ harmonic/chains.pyx
@@ -2,37 +2,46 @@ class Chains:
     """Class to store samples from multiple MCMC chains.    
     """
     
-    def __init__(self, samples=empty ndarray):
-        self.nchains = 1        
-        self.ndim, self.nsamples = samples.shape
-        self.samples = samples
-        self.chain_start_indices = [0] #ndarray?
-        self.chain_start_indices.append(self.nsamples)
+    
+    def __init__(self, samples=empty ndarray):        
+        pass
+        
+        # self.nchains = 1        
+        # self.ndim, self.nsamples = samples.shape
+        # self.samples = samples
+        # self.chain_start_indices = [0] #ndarray?
+        # self.chain_start_indices.append(self.nsamples)
         
         
     def add_chain(self, samples):
+        pass
+        
         # check dimension correct
-        self.nchains += 1
-        self.samples.append(samples)
-        self.nsamples += samples.shape[0] # right dimension?
-        self.chain_start_indices.append(self.nsamples)
-    
+        # self.nchains += 1
+        # self.samples.append(samples)
+        # self.nsamples += samples.shape[0] # right dimension?
+        # self.chain_start_indices.append(self.nsamples)
+        # 
     
     def add_chains(self, samples):
+        pass
         
         
     def add_chains_3d(self, samples):
+        pass
         
         
     
     
     
     def get_chain(i):
+        pass
         # check i valid
-        return self.samples[self.chain_start_indices[i]:
-                            self.chain_start_indices[i+1]]                            
+        # return self.samples[self.chain_start_indices[i]:
+        #                     self.chain_start_indices[i+1]]                            
                             
-    def get_nsamples_in_chain(i):    
+    def get_nsamples_in_chain(i): 
+        pass   
         # check i valid        
-        return (self.chain_start_indices[i+1] - self.chain_start_indices[i])
+        # return (self.chain_start_indices[i+1] - self.chain_start_indices[i])
                             
