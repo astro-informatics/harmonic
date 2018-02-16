75423ce4d15c5fa1bc9f38cad7fa4a85738594c1
Christopher Wallis <cwallis@Christophers-MacBook-Pro.local>
2018-02-16 14:20:22 +0000

    appeneded model class with hyper sphere

--- harmonic/model.pyx
+++ harmonic/model.pyx
@@ -22,3 +22,18 @@ class ModelBase(metaclass=abc.ABCMeta):
         """Use model to predict the hight of the posterior at point x
         """
 
+class HyperSphere(ModelBase):
+
+    def __init__(self, ranges, hyper_parameters=None):
+        """ constructor setting the hyper parameters of the model
+        """
+
+    def fit(self, np.ndarray[double, ndim=2, mode="c"] X, np.ndarray[double, ndim=1, mode="c"] Y):
+        """Fit the parameters of the model
+        """
+        return
+
+    # def predict(self, np.ndarray[double, ndim=1, mode="c"] x):
+    #     """Use model to predict the hight of the posterior at point x
+    #     """
+    #     return
