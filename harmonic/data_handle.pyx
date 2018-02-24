14c2c9e6f096c48c42d84e7a3aa7b7b975a4060d
Christopher Wallis <cwallis@Christophers-MacBook-Pro.local>
2018-02-24 10:57:12 +0000

    changed to spaces

--- harmonic/data_handle.pyx
+++ harmonic/data_handle.pyx
@@ -8,70 +8,72 @@ import chains as ch
 
 
 def split_data(chains not None, split_ratio=0.5):
-	"""splits the data in a chains instance into two
-	   so the new chains instances can be used for testing
-	   and calculationg the evidence.
+    """splits the data in a chains instance into two
+       so the new chains instances can be used for testing
+       and calculationg the evidence.
 
-	Args:
-		chains: instance of a chains class with the data 
-			to be split
-		split_ratio: The ratio of the data to be used in
-			training (default=0.5)
+    Args:
+        chains: instance of a chains class with the data 
+            to be split
+        split_ratio: The ratio of the data to be used in
+            training (default=0.5)
 
-	Returns:
-		chains_train: instance of a chains class to be used
-			to fit the model
-		chains_use: instance of a chains class to be used
-			to calculate the evidence
+    Returns:
+        chains_train: instance of a chains class to be used
+            to fit the model
+        chains_use: instance of a chains class to be used
+            to calculate the evidence
 
 
 
-	"""
+    """
 
-	nchains_train = int(chains.nchains * split_ratio)
-	nchains_use   = chains.nchains - nchains_train
+    nchains_train = int(chains.nchains * split_ratio)
+    nchains_use   = chains.nchains - nchains_train
 
-	ndim = chains.ndim
+    ndim = chains.ndim
 
-	chains_train = ch.Chains(ndim)
-	chains_use   = ch.Chains(ndim)
+    chains_train = ch.Chains(ndim)
+    chains_use   = ch.Chains(ndim)
 
-	start_index = chains.start_indices[0]
-	end_index   = chains.start_indices[nchains_train]
-	chains_train.add_chains_2d_list(chains.samples[start_index:end_index,:],\
-									chains.ln_posterior[start_index:end_index],\
-									nchains_train, \
-									chains.start_indices[:nchains_train+1])
+    start_index = chains.start_indices[0]
+    end_index   = chains.start_indices[nchains_train]
+    chains_train.add_chains_2d_list(chains.samples[start_index:end_index,:],\
+                                    chains.ln_posterior[start_index:end_index],\
+                                    nchains_train, \
+                                    chains.start_indices[:nchains_train+1])
 
-	start_index = chains.start_indices[nchains_train]
-	end_index   = chains.start_indices[-1]
-	chains_use.add_chains_2d_list(chains.samples[start_index:end_index,:],\
-									chains.ln_posterior[start_index:end_index],\
-									nchains_use, \
-									chains.start_indices[nchains_train:])
+    start_index = chains.start_indices[nchains_train]
+    end_index   = chains.start_indices[-1]
+    chains_use.add_chains_2d_list(chains.samples[start_index:end_index,:],\
+                                    chains.ln_posterior[start_index:end_index],\
+                                    nchains_use, \
+                                    chains.start_indices[nchains_train:])
 
-	return chains_train, chains_use
+    return chains_train, chains_use
 
 
 def cross_validation(chains, domains, hyper_parameters, ncross=2, MODEL="KernalDensityEstimation"):
-	""" Splits data into ncross chunks. Then fits the model using
-		each of the hyper parameters given using all but one of the 
-		chunks. This procedure is done for all the chunks and the 
-		average varience from all the chunks is used to decide which
-		hyper parameters list was better.
+    """ Splits data into ncross chunks. Then fits the model using
+        each of the hyper parameters given using all but one of the 
+        chunks. This procedure is done for all the chunks and the 
+        average varience from all the chunks is used to decide which
+        hyper parameters list was better.
 
-	Args:
-		chains: instance of a chains class with the data 
-			trianed on
-		domains: The domains of the model's parameters
-		hyper_parameters: A list of length ncross where each entry
-			is a hyper_parameters list to be trialed
+    Args:
+        chains: instance of a chains class with the data 
+            trianed on
+        domains: The domains of the model's parameters
+        hyper_parameters: A list of length ncross where each entry
+            is a hyper_parameters list to be trialed
 
-	Returns:
-		hyper_parameter list that was most succesful
+    Returns:
+        hyper_parameter list that was most succesful
 
-		Raises:
-	"""
+        Raises:
+    """
 
-	if MODEL == "KernalDensityEstimation":
-		print("using ", MODEL)
+    
+
+    if MODEL == "KernalDensityEstimation":
+        model = md.KernalDensityEstimation(chains.ndim, domains, hyper_parameters[i_val])
