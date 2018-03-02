09b66493484319acd7488254fa0547d57fb262a2
Christopher Wallis <cwallis@eduroam-int-dhcp-97-41-206.ucl.ac.uk>
2018-03-02 16:17:25 +0000

    improved plotting

--- examples/plot_example.py
+++ examples/plot_example.py
@@ -11,16 +11,22 @@ args = parser.parse_args()
 
 rho_array = np.loadtxt(args.base_name+".dat")
 
-print(np.exp(results.ln_rho))
-print(rho_array[:,0])
 
 rho_mean         = np.mean(rho_array[:,0])
 rho_std_measured = np.std(rho_array[:,0])
+rho_var_mean_est = np.mean(rho_array[:,1])
+rho_var_std_meas = np.std(rho_array[:,1])
 rho_std_mean_est = np.sqrt(np.mean(rho_array[:,1]))
+rho_var_var_mean = np.mean(rho_array[:,2])
 
-
+plt.figure()
 plt.plot(np.arange(3),np.zeros(3)+np.exp(results.ln_rho))
 plt.plot(np.zeros(results.n_real)+1,rho_array[:,0],'k+')
 plt.errorbar(np.zeros(1)+0.9,rho_mean,yerr=rho_std_measured, fmt='--o')
 plt.errorbar(np.zeros(1)+1.1,rho_mean,yerr=rho_std_mean_est, fmt='--o')
+
+plt.figure()
+# plt.plot(np.arange(3),np.ones(3))
+plt.errorbar(np.zeros(1)+0.9,rho_var_mean_est,yerr=rho_var_std_meas, fmt='--o')
+plt.errorbar(np.zeros(1)+1.1,rho_var_mean_est,yerr=np.sqrt(rho_var_var_mean), fmt='--o')
 plt.show()
