bc93f490ec13c4ae33cccfa718ff98e67d200306
Jason McEwen <jason.mcewen@gmail.com>
2020-07-09 17:17:37 +0100

    Remove optimsation experiments

--- harmonic/__init__.py
+++ harmonic/__init__.py
@@ -1,6 +1,6 @@
 from .chains import Chains
 from . import evidence
-from .evidence import Evidence, Optimisation, Shifting
+from .evidence import Evidence, Shifting
 from . import model
 from . import utils
 from . import logs
