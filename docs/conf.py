044a41be0c94b14489cb85fed6a2139565023237
Matthew Docherty <matthew.docherty.21@ucl.ac.uk>
2021-11-29 08:43:36 +0000

    Update to v1.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2021, Jason D. McEwen, Christopher G. R. Wallis, Matthew A. Price, 
 author = 'Jason D. McEwen, Christopher G. R. Wallis, Matthew A. Price, Matthew M. Docherty'
 
 # The short X.Y version
-version = '1.0.2'
+version = '1.0.3'
 # The full version, including alpha/beta/rc tags
-release = '1.0.2'
+release = '1.0.3'
 
 
 # -- General configuration ---------------------------------------------------
