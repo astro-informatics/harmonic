8bc85717a0ca138fcc73648c866e7958fd607868
Matthew Docherty <matthew.docherty.21@ucl.ac.uk>
2022-06-13 11:45:42 +0100

    Revert to older python versions

--- setup.py
+++ setup.py
@@ -34,11 +34,9 @@ extra_link_args=[]
 
 setup(
     classifiers=[
-        # 'Programming Language :: Python :: 3.6',
-        # 'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research'
