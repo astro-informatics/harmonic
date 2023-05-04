eed0d7682801cab6ed4af58124ed86a258ddbaf5
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2023-05-04 18:36:12 +0100

    Refactor NVP flow.

--- harmonic/flows.py
+++ harmonic/flows.py
@@ -1,3 +1,4 @@
+from typing import Sequence
 import numpy as np
 import flax.linen as nn
 import optax
@@ -7,6 +8,8 @@ import jax
 import jax.numpy as jnp
 import tensorflow_probability as tfp
 import flowMC
+from flax.training import train_state
+from tqdm import trange
 
 tfp = tfp.substrates.jax
 tfd = tfp.distributions
@@ -23,7 +26,6 @@ class AffineCoupling(nn.Module):
 
     @nn.compact
     def __call__(self, x, nunits):
-        net = nn.leaky_relu(nn.Dense(128)(x))
         net = nn.leaky_relu(nn.Dense(128)(x))
 
         # Shift parameter:
@@ -80,3 +82,91 @@ class NVPFlowSampler(nn.Module):
     def __call__(self, key, n_samples, scale=1.0):
         nvp = make_nvp_fn(scale=scale)
         return nvp.sample(n_samples, seed=key)
+
+
+class RealNVP(nn.Module):
+    """
+    Real NVP flow using flax and tfp-jax.
+    """
+
+    n_features: int
+    hidden_layers: Sequence[int]
+    frac_masked: float = 0.5
+
+    def setup(self):
+        self.affine1 = AffineCoupling(name="affine1")
+        self.affine2 = AffineCoupling(name="affine2")
+        self.affine3 = AffineCoupling(name="affine3", apply_scaling=False)
+        self.affine4 = AffineCoupling(name="affine4", apply_scaling=False)
+        self.affine5 = AffineCoupling(name="affine5", apply_scaling=False)
+        self.affine6 = AffineCoupling(name="affine6", apply_scaling=False)
+
+    def make_flow(self, var_scale=1.0):
+
+        """affine_coupling = tfb.real_nvp_default_template(
+            hidden_layers=self.hidden_layers, activation=nn.leaky_relu
+        )
+        affine_coupling_unscaled = tfb.real_nvp_default_template(
+            hidden_layers=self.hidden_layers, activation=nn.leaky_relu, shift_only=True
+        )"""
+
+        # Computes the likelihood of these x
+        chain = tfb.Chain(
+            [
+                tfb.RealNVP(fraction_masked=self.frac_masked, bijector_fn=self.affine1),
+                tfb.Permute([1, 0]),
+                tfb.RealNVP(fraction_masked=self.frac_masked, bijector_fn=self.affine2),
+                tfb.Permute([1, 0]),
+                tfb.RealNVP(
+                    fraction_masked=self.frac_masked,
+                    bijector_fn=self.affine3,
+                ),
+                tfb.Permute([1, 0]),
+                tfb.RealNVP(
+                    fraction_masked=self.frac_masked,
+                    bijector_fn=self.affine4,
+                ),
+                tfb.Permute([1, 0]),
+                tfb.RealNVP(
+                    fraction_masked=self.frac_masked,
+                    bijector_fn=self.affine5,
+                ),
+                tfb.Permute([1, 0]),
+                tfb.RealNVP(
+                    fraction_masked=self.frac_masked,
+                    bijector_fn=self.affine6,
+                ),
+            ]
+        )
+
+        nvp = tfd.TransformedDistribution(
+            distribution=tfd.MultivariateNormalDiag(
+                loc=jnp.zeros(self.n_features), scale_identity_multiplier=var_scale
+            ),
+            bijector=chain,
+        )
+
+        return nvp
+
+    def __call__(self, x, var_scale=1.0) -> jnp.array:
+        # x = (x - self.base_mean.value) / jnp.sqrt(jnp.diag(self.base_cov.value))
+        flow = self.make_flow(var_scale=var_scale)
+        return flow.log_prob(x)
+
+    def sample(
+        self, rng: jax.random.PRNGKey, num_samples: int, var_scale: float = 1.0
+    ) -> jnp.array:
+        """ "
+        Sample from the flow.
+        """
+        nvp = self.make_flow(var_scale=var_scale)
+        samples = nvp.sample(num_samples, seed=rng)
+
+        return samples
+
+    def log_prob(self, x: jnp.array, var_scale: float = 1.0) -> jnp.array:
+
+        get_logprob = jax.jit(jax.vmap(self.__call__, in_axes=[0, None]))
+        logprob = get_logprob(x, var_scale)
+
+        return logprob
--- harmonic/model_nf.py
+++ harmonic/model_nf.py
@@ -1,3 +1,4 @@
+from typing import Sequence, Callable
 import model as md
 import pickle
 import numpy as np
@@ -8,10 +9,13 @@ import optax
 from functools import partial
 import tqdm
 import flowMC
-from flowMC.nfmodel.utils import make_training_loop, sample_nf
+
+from flowMC.nfmodel.utils import make_training_loop
 from flowMC.nfmodel.rqSpline import RQSpline
 import flax
 from flax.training import train_state  # Useful dataclass to keep train state
+import distrax
+import flax.linen as nn
 
 
 class NormalizingFlow(md.Model):
@@ -350,6 +354,216 @@ class RQSplineFlow(md.Model):
             x,
             scaling,
             method=self.flow.log_prob_scaled,
+        )
+
+        return logprob
+
+    def sample(self, n_sample, rng_key=jax.random.PRNGKey(0), compressed=True):
+        """Sample n_sample samples from trained flow."""
+
+        rng_key, subkey = jax.random.split(rng_key)
+
+        if compressed:
+            scaling = self.scaling
+        if not compressed:
+            scaling = 1.0
+
+        samples = self.flow.apply(
+            {"params": self.state.params, "variables": self.variables},
+            subkey,
+            n_sample,
+            scaling,
+            method=self.flow.sample,
+        )
+
+        return samples
+
+    def is_fitted(self):
+        """Specify whether model has been fitted.
+
+        Returns:
+
+            (bool): Whether the model has been fitted.
+
+        """
+
+        return self.fitted
+
+    def serialize(self, filename):
+        """Serialize Model object.
+
+        Args:
+
+            filename (string): Name of file to save model object.
+
+        """
+
+        file = open(filename, "wb")
+        pickle.dump(self, file)
+        file.close()
+
+        return
+
+    def deserialize(self, filename):
+        """Deserialize Model object from file.
+
+        Args:
+
+            filename (string): Name of file from which to read model object.
+
+        Returns:
+
+            (Model): Model object deserialized from file.
+
+        """
+        file = open(filename, "rb")
+        model = pickle.load(file)
+        file.close()
+
+        return model
+
+
+class RealNVP(md.Model):
+    """Normalizing flow model to approximate the log_e posterior by a normalizing flow."""
+
+    def __init__(
+        self,
+        ndim_in,
+        hidden_layers=[128, 128],
+        scaling=0.8,
+        learning_rate=0.001,
+        momentum=0.9,
+        hyper_parameters=None,
+    ):
+        """Constructor setting the hyper-parameters and domains of the model.
+
+        Must be implemented by derived class (currently abstract).
+
+        Args:
+
+            ndim (int): Dimension of the problem to solve.
+
+            domains (list): List of 1D numpy ndarrays containing the domains
+                for each parameter of model. Each domain is of length two,
+                specifying a lower and upper bound for real hyper-parameters
+                but can be different in other cases if required.
+
+            flow (Flow): Normalizing flow used to approximate the posterior.
+
+            scaling (float): Scale factor by which the base distribution Gaussian
+                is compressed in the prediction step. Should be positive and <=1.
+
+            hyper_parameters (list): Hyper-parameters for model.
+
+        Raises:
+
+            ValueError: If the ndim_in is not positive.
+            ValueError: If scaling is negative or greater than 1.
+
+        """
+
+        if ndim_in < 1:
+            raise ValueError("Dimension must be greater than 0.")
+
+        if scaling > 1:
+            raise ValueError("Scaling must not be greater than 1.")
+
+        if scaling <= 0:
+            raise ValueError("Scaling must be positive.")
+
+        self.ndim = ndim_in
+        self.fitted = False
+        self.scaling = scaling
+        self.state = None
+
+        # Model parameters
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.flow = flows.RealNVP(ndim_in, hidden_layers)
+
+    def create_train_state(self, rng):
+        params = self.flow.init(rng, jnp.ones((1, self.ndim)))["params"]
+        tx = optax.adam(self.learning_rate, self.momentum)
+        return train_state.TrainState.create(
+            apply_fn=self.flow.apply, params=params, tx=tx
+        )
+
+    def fit(self, X, Y, batch_size=10000, epochs=3, key=jax.random.PRNGKey(1000)):
+        """Fit the parameters of the model.
+
+        Args:
+
+            X (double ndarray[nsamples, ndim]): Sample x coordinates.
+
+            Y (double ndarray[nsamples]): Target log_e posterior values for each
+                sample in X.
+
+        Returns:
+
+            (bool): Whether fit successful.
+
+
+        Raises:
+
+            ValueError: Raised if the first dimension of X is not the same as
+                Y.
+
+            ValueError: Raised if the second dimension of X is not the same as
+                ndim.
+
+        """
+
+        if X.shape[0] != Y.shape[0]:
+            raise ValueError("X and Y sizes are not the same.")
+
+        if X.shape[1] != self.ndim:
+            raise ValueError("X second dimension not the same as ndim.")
+
+        key, rng_model, rng_init, rng_train, rng_nf_sample = jax.random.split(key, 5)
+
+        init = self.flow.init(rng_model, jnp.ones((1, self.ndim)))
+        print(init.keys())
+        variables = init
+
+        state = self.create_train_state(rng_init)
+
+        train_flow, train_epoch, train_step = make_training_loop(self.flow)
+        rng, state, loss_values = train_flow(
+            rng_train, state, variables, X, epochs, batch_size
+        )
+
+        self.state = state
+        self.variables = variables
+        self.fitted = True
+
+        return
+
+    def predict(self, x, compressed=True):
+        """Predict the value of the posterior at point x.
+
+        Must be implemented by derived class (since abstract).
+
+        Args:
+
+            x (double ndarray[ndim]): Sample of shape (ndim) at which to
+                predict posterior value.
+
+        Returns:
+
+            (double): Predicted log_e posterior value.
+
+        """
+
+        if compressed:
+            scaling = self.scaling
+        if not compressed:
+            scaling = 1.0
+
+        logprob = self.flow.apply(
+            {"params": self.state.params, "variables": self.variables},
+            x,
+            scaling,
+            method=self.flow.log_prob,
         )
 
         return logprob
