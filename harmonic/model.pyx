2c886a6866e5991eec1ab4bc0208839cf1766f7c
CosmoMatt <mattaprice@hotmail.co.uk>
2023-07-24 09:57:06 +0100

    explicitly cast to long in model.pyx

--- harmonic/model.pyx
+++ harmonic/model.pyx
@@ -839,7 +839,7 @@ class KernelDensityEstimate(Model):
         for i_dim in range(ndim):
             sub_index = <long>((x[i_dim]-start_end[i_dim,0]) * \
                 inv_scales[i_dim]*inv_diam) + 1
-            index += sub_index*ngrid**i_dim
+            index += <long>(sub_index*ngrid**i_dim)
 
         KernelDensityEstimate_loop_round_and_search(index, i_dim, ngrid, ndim, 
                                                     grid, samples, x, 
