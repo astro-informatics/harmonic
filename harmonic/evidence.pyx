576aca1e8df602a679f120b66ea091f99acfe4d0
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2023-08-09 14:51:46 +0100

    Refactor changing flow temperature to be performed in model class.

--- harmonic/evidence.pyx
+++ harmonic/evidence.pyx
@@ -30,7 +30,7 @@ class Evidence:
     """
 
     def __init__(self, long nchains, model not None, \
-                 shift=Shifting.MEAN_SHIFT):
+                 shift=Shifting.MEAN_SHIFT, batch_calculation=False):
         """Construct evidence class for computing inverse evidence values from
         set number of chains and initialised posterior model.
 
@@ -44,6 +44,9 @@ class Evidence:
 
             shift (Shifting): What shifting method to use to avoid over/underflow during
                 computation. Selected from enumerate class.
+
+            batch_calculation (bool): Set to True to predict posterior for the whole batch 
+                at the same time when using normalizing flows. Defaults to False.
 
         Raises:
 
@@ -91,6 +94,7 @@ class Evidence:
         self.chains_added = False
 
         self.model = model
+        self.batch_calculation = batch_calculation
 
         # Technical details
         self.lnargmax = -np.inf
@@ -204,7 +208,7 @@ class Evidence:
         return
 
 
-    def add_chains(self, chains not None, bulk_calc: bool =False, var_scale: float = 0.8):
+    def add_chains(self, chains not None):
         """Add new chains and calculate an estimate of the inverse evidence, its
         variance, and the variance of the variance.
 
@@ -219,11 +223,6 @@ class Evidence:
 
             chains: An instance of the chains class containing the chains to
                 be used in the calculation.
-
-            bulk_calc (bool): Set to True to predict posterior for the whole batch at the same time when using normalizing flows. Defaults to False.
-
-            var_scale (float): Scale factor by which the base distribution Gaussian
-                is compressed in the prediction step. Should be positive and <=1.
 
         Raises:
 
@@ -252,15 +251,15 @@ class Evidence:
         cdef double mean_shift, max_shift, max_i
 
         lnargs = np.zeros_like(Y)
-        if bulk_calc:
-            lnpred = self.model.predict(x=X, var_scale=var_scale)
+        if self.batch_calculation:
+            lnpred = self.model.predict(x=X)
             
         for i_chains in range(nchains):
             i_samples_start = chains.start_indices[i_chains]
             i_samples_end = chains.start_indices[i_chains+1]
             
             for i,i_samples in enumerate(range(i_samples_start, i_samples_end)):
-                if bulk_calc:
+                if self.batch_calculation:
                     lnpredict = lnpred[i_samples]
                 else:
                     lnpredict = self.model.predict(X[i_samples,:])
