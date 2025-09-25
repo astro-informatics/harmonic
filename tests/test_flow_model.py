c5fdfa25c962da6d70cbbce63688fc685148fc30
alicjapolanska <alicja.polanska.22@ucl.ac.uk>
2025-09-25 17:09:51 +0100

    Change key

--- tests/test_flow_model.py
+++ tests/test_flow_model.py
@@ -218,7 +218,7 @@ def test_flows_gaussian(model):
         epochs = 20
 
     # Initialize a PRNG key (you can use any valid key)
-    key = jax.random.PRNGKey(0)
+    key = jax.random.PRNGKey(10)
     mean = jnp.zeros(ndim)
     cov = jnp.eye(ndim)
 
